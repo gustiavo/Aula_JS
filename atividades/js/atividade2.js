8e6a777bb373291999869fcadb4a3c4ae7a7660d
gstvera <gustiavow@gmail.com>
2023-11-21 11:51:52 -0300

    up
    up

--- atividades/js/atividade2.js
+++ atividades/js/atividade2.js
@@ -1,9 +1,14 @@
-confirm("Sucessor e Antecessor")
+// confirm("Sucessor e Antecessor")
 
-var num = Number(prompt("Digite o seu número "))
+// var num = Number(prompt("Digite o seu número "))
 
-var ant = num - 1
-var suc = num + 1
+// var ant = num - 1
+// var suc = num + 1
 
-alert(`o antecessor ${ant}, seu número ${num}, o sucessor ${suc}`)
+// alert(`o antecessor ${ant}, seu número ${num}, o sucessor ${suc}`)
+
+var numero = Number(prompt("Qual é o número desejado"))
+
+alert(`O antecessor do numero ${numero - 1}, O seu número é ${numero}, o sucessor é ${numero + 1}`)
+
 
--- atividades/js/script.js
+++ atividades/js/script.js
@@ -1,6 +1,18 @@
-confirm("Deseja encerrar sua conta?")
-var numClientes = Number(prompt("Digite a quantidade de pessoas na mesa"))
-var valorConta = Number(prompt("Digite o valor da conta"))
+// confirm("Deseja encerrar sua conta?")
+// var numClientes = Number(prompt("Digite a quantidade de pessoas na mesa"))
+// var valorConta = Number(prompt("Digite o valor da conta"))
 
-var contaDividida =  valorConta / numClientes
-alert(`O valor final da conta é de R$ ${contaDividida}`)
+// var contaDividida =  valorConta / numClientes
+// alert(`O valor final da conta é de R$ ${contaDividida}`)
+
+function calculeConta(numClientes, valorConta){
+    alert ("Calcula conta")
+    
+    let contaDividida =  valorConta / numClientes
+    
+    alert(`O valor final da conta é de R$ ${contaDividida}`)
+    
+}
+    var numClientes = Number(prompt("Digite a quantidade de pessoas na mesa"))
+    var valorConta = Number(prompt("Digite o valor da conta"))
+    calculeConta(numClientes, valorConta)
