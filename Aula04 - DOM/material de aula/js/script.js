d7dfcec756595e8071608946353a2cf88247028d
Gustavo <gustiavow@gmail.com>
2023-11-24 11:42:41 -0300

    up
    up

--- Aula04 - DOM/material de aula/js/script.js
+++ Aula04 - DOM/material de aula/js/script.js
@@ -9,15 +9,19 @@ function calcular(){
     let filhos = document.querySelector("#filhos").value
     let salario = document.querySelector("#salario").value
     let resultadoSpan = document.querySelector("#resultado")
-    if(salario <= 806.80){
-        let resultado = filhos * 41.37;
-        resultadoSpan.innerHTML = `O salário-família do empregado ${empregado} é de ${resultado}`
-    }else{
-        if(salario >= 806.81 && salario <= 1212.64){
-            let resultado = filhos * 29.16;
-        resultadoSpan.innerHTML = `O salário-família do empregado ${empregado} é de ${resultado}`
+
+    if(filhos == 0 || salario == 0 || empregado``){
+        resultadoSpan.innetHTML = `Dados inválidos! Insira as informações corretamente!`
+        if(salario <= 806.80){
+            let resultado = filhos * 41.37;
+            resultadoSpan.innerHTML = `O salário-família do empregado ${empregado} é de ${resultado}`
         }else{
-            resultadoSpan.innerHTML = `Não há salário família para o empregado`
+            if(salario >= 806.81 && salario <= 1212.64){
+                let resultado = filhos * 29.16;
+            resultadoSpan.innerHTML = `O salário-família do empregado ${empregado} é de ${resultado}`
+            }else{
+                resultadoSpan.innerHTML = `Não há salário família para o empregado ${empregado}`
+            }
+        }  
         }
-    }  
-}
+     }
